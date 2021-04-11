71d3c18e692a998c32e7ad31390de1d5c2a1578b
Hugo Berra <53909003+jtberra@users.noreply.github.com>
2021-04-11 10:50:54 -0500

    protección de todas las rutas actuales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { SendEmailComponent } from './componentes/send-email/send-email.component';
 import { GuardEditGuard } from './guards/guard-edit.guard';
 
-const routes: Routes = [{ 
-    path: 'home', 
-    loadChildren: () => import('./modulos/home/home.module').then(m => m.HomeModule),
-    canActivate:[GuardEditGuard],
-
-  },
-  {
+const routes: Routes = [{
     path: '', 
     redirectTo: './modulos/home', 
     pathMatch: 'full'
-  }, 
+  },
+  { 
+    path: 'home', 
+    loadChildren: () => import('./modulos/home/home.module').then(m => m.HomeModule),
+    canActivate:[GuardEditGuard],
+  },
   { 
     path: 'login', 
     loadChildren: () => import('./modulos/login/login.module').then(m => m.LoginModule) 
@@ -26,12 +25,30 @@ const routes: Routes = [{
     path: 'verification-email',
     component: SendEmailComponent
   },
-    { path: 'forgot-password', loadChildren: () => import('./modulos/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) },
-    { path: 'nuevo-curso', loadChildren: () => import('./cursos/nuevo-curso/nuevo-curso.module').then(m => m.NuevoCursoModule) },
-    { path: 'cursos-impartidos', loadChildren: () => import('./cursos/cursos-impartidos/cursos-impartidos.module').then(m => m.CursosImpartidosModule) },
-    { path: 'cursos-asignados', loadChildren: () => import('./cursos/cursos-asignados/cursos-asignados.module').then(m => m.CursosAsignadosModule) },
-    { path: 'modificar-curso', loadChildren: () => import('./cursos/modificar-curso/modificar-curso.module').then(m => m.ModificarCursoModule) },
-    { path: 'detalles-curso', loadChildren: () => import('./cursos/detalles-curso/detalles-curso.module').then(m => m.DetallesCursoModule) }];
+  { path: 'forgot-password', 
+    loadChildren: () => import('./modulos/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) 
+  },
+  { 
+    path: 'nuevo-curso', 
+    loadChildren: () => import('./cursos/nuevo-curso/nuevo-curso.module').then(m => m.NuevoCursoModule),
+    canActivate:[GuardEditGuard]
+  },
+  { path: 'cursos-impartidos', 
+    loadChildren: () => import('./cursos/cursos-impartidos/cursos-impartidos.module').then(m => m.CursosImpartidosModule),
+    canActivate:[GuardEditGuard]
+  },
+  { path: 'cursos-asignados', 
+    loadChildren: () => import('./cursos/cursos-asignados/cursos-asignados.module').then(m => m.CursosAsignadosModule),
+    canActivate:[GuardEditGuard]
+  },
+  { path: 'modificar-curso', 
+    loadChildren: () => import('./cursos/modificar-curso/modificar-curso.module').then(m => m.ModificarCursoModule), 
+    canActivate:[GuardEditGuard]
+  },
+  { path: 'detalles-curso', 
+    loadChildren: () => import('./cursos/detalles-curso/detalles-curso.module').then(m => m.DetallesCursoModule),
+    canActivate:[GuardEditGuard]
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
